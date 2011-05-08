e015452c064cda9ed02f7f2722a2b10dd84baf0b
Erik LaBianca <erik.labianca@gmail.com>
2011-05-07 23:32:39 -0400

    put docbook xslt sheets in lib_managed"

--- src/main/scala/easel/DocBookPlugin.scala
+++ src/main/scala/easel/DocBookPlugin.scala
@@ -8,7 +8,8 @@ trait DocBookPlugin extends DefaultProject {
   val docBookVersion = "1.76.1"
   val docBookXslUrl = new URL("http://sourceforge.net/projects/docbook/files/docbook-xsl/" + 
     docBookVersion + "/docbook-xsl-" + docBookVersion + ".zip/download")
-  val docBookXslRoot:Path = managedDependencyPath / ("docbook-xsl-" + docBookVersion)
+  val docBookXslRoot:Path = managedDependencyPath
+  val docBookXslPath:Path = docBookXslRoot / ("docbook-xsl-" + docBookVersion)
 
 
   lazy val compileDocBook = dynamic(compileDocBookAction) describedAs "Compiles DocBook"
@@ -20,13 +21,19 @@ trait DocBookPlugin extends DefaultProject {
   }
   
   lazy val updateDocBook = dynamic(updateDocBookAction) describedAs "Updates DocBook"
-  def updateDocBookAction = task{ updateDocBookTask }.named("update-docbook")
-  override def updateAction = super.updateAction.dependsOn(updateDocBookAction)
+  def updateDocBookAction = task{ updateDocBookTask }.named("update-docbook").dependsOn(super.updateAction)
+  override def updateAction = updateDocBookAction
 
   def updateDocBookTask: Option[String] = { 
-		log.info("Acquiring docbook xsl from " + docBookXslUrl)
+    log.info("Updating docbook stylesheets in " + docBookXslRoot)
+
 		if (!docBookXslRoot.isDirectory) {
-		  FileUtilities.unzip(docBookXslUrl, outputRootPath, log).left.foreach(error)
+	      log.info("Creating " + docBookXslRoot)
+			  FileUtilities.createDirectory(docBookXslRoot, log)
+	  }
+		if (!docBookXslPath.isDirectory) {
+	  	log.info("Acquiring docbook xsl from " + docBookXslUrl + ", extracting to " + docBookXslPath)
+		  FileUtilities.unzip(docBookXslUrl, docBookXslRoot, log).left.foreach(error)
 		}	
 		None
   } 
