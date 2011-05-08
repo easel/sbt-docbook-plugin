3d60dff7fcd1bc8437d720d905f284a0149f6244
Erik LaBianca <erik.labianca@gmail.com>
2011-05-07 23:51:46 -0400

    try to figure out why docBookXslRoot.isDirectory is false

--- src/main/scala/easel/DocBookPlugin.scala
+++ src/main/scala/easel/DocBookPlugin.scala
@@ -1,4 +1,7 @@
 package easel
+import javax.xml.transform.{TransformerFactory, Transformer}
+import javax.xml.transform.stream.{StreamSource, StreamResult}
+import java.io.FileOutputStream
 import java.net.URL
 import sbt._
 
@@ -8,18 +11,9 @@ trait DocBookPlugin extends DefaultProject {
   val docBookVersion = "1.76.1"
   val docBookXslUrl = new URL("http://sourceforge.net/projects/docbook/files/docbook-xsl/" + 
     docBookVersion + "/docbook-xsl-" + docBookVersion + ".zip/download")
-  val docBookXslRoot:Path = managedDependencyPath
-  val docBookXslPath:Path = docBookXslRoot / ("docbook-xsl-" + docBookVersion)
+  lazy val docBookXslRoot:Path = managedDependencyPath
+  lazy val docBookXslPath:Path = docBookXslRoot / ("docbook-xsl-" + docBookVersion)
 
-
-  lazy val compileDocBook = dynamic(compileDocBookAction) describedAs "Compiles DocBook"
-  def compileDocBookAction = task{ compileDocBookTask }.named("compile-docbook")
-  override def compileAction = super.compileAction.dependsOn(compileDocBook)
-  def compileDocBookTask: Option[String] = {
-		log.info("Compiling docbook")
-		None
-  }
-  
   lazy val updateDocBook = dynamic(updateDocBookAction) describedAs "Updates DocBook"
   def updateDocBookAction = task{ updateDocBookTask }.named("update-docbook").dependsOn(super.updateAction)
   override def updateAction = updateDocBookAction
@@ -27,6 +21,7 @@ trait DocBookPlugin extends DefaultProject {
   def updateDocBookTask: Option[String] = { 
     log.info("Updating docbook stylesheets in " + docBookXslRoot)
 
+    log.info(docBookXslRoot.isDirectory.toString())
 		if (!docBookXslRoot.isDirectory) {
 	      log.info("Creating " + docBookXslRoot)
 			  FileUtilities.createDirectory(docBookXslRoot, log)
@@ -36,7 +31,29 @@ trait DocBookPlugin extends DefaultProject {
 		  FileUtilities.unzip(docBookXslUrl, docBookXslRoot, log).left.foreach(error)
 		}	
 		None
-  } 
+  }
+
+
+  lazy val compileDocBook = dynamic(compileDocBookAction) describedAs "Compiles DocBook"
+  def compileDocBookAction = task{ compileDocBookTask }.named("compile-docbook")
+  override def compileAction = super.compileAction.dependsOn(compileDocBook)
+  def compileDocBookTask: Option[String] = {
+		log.info("Compiling docbook")
+		
+		// 1. Instantiate a TransformerFactory.
+		val tFactory = TransformerFactory.newInstance();
+
+		// 2. Use the TransformerFactory to process the stylesheet Source and
+		//    generate a Transformer.
+		val transformer = tFactory.newTransformer(new StreamSource("foo.xsl"));
+
+		// 3. Use the Transformer to transform an XML Source and send the
+		//    output to a Result object.
+		transformer.transform(new StreamSource("foo.xml"), new StreamResult( new FileOutputStream("foo.out")));
+		None
+  }
+  
+
 
 
 }
